41712032e65f6e55a8ee7d16f88ca612a368b120
Prashant B <prashant_bhikhu@hotmail.com>
2017-05-23 13:03:20 +1200

    Added functions userExists(...) and getUser(...).

--- src/main/UserDatabase.java
+++ src/main/UserDatabase.java
@@ -75,4 +75,21 @@ public class UserDatabase {
         }
     }
 
+    public boolean userExists(String username) {
+        for(User user: users) {
+            if(user.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public User getUser(String username) {
+        for(User user: users) {
+            if(user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
 }
