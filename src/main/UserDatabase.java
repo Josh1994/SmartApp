9ae0980677d6392cba468d8c382f3ff86b5f1999
Prashant B <prashant_bhikhu@hotmail.com>
2017-05-23 12:55:13 +1200

    Added User Database Class.

---
+++ src/main/UserDatabase.java
@@ -0,0 +1,68 @@
+package main;
+
+import model.User;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Prashant on 23/05/2017.
+ */
+public class UserDatabase {
+    private static final String USER_DATABASE = "user.config";
+
+    private List<User> users;
+    private User loggedIn;
+
+    public UserDatabase() {
+        loadUserDatabase();
+    }
+
+    private void loadUserDatabase() {
+        // Declare class variables
+        users = new ArrayList<>();
+
+        File userDatabaseFile = new File(USER_DATABASE);
+
+        // Create Initial User Database if it doesn't exist yet
+        if(!userDatabaseFile.exists()) {
+            createInitialDatabase();
+        }
+
+        try {
+            Scanner scanner = new Scanner(userDatabaseFile);
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] lineSplit = line.split(",");
+
+
+                String username = lineSplit[0];
+                String password = lineSplit[1];
+                boolean isManager = Boolean.parseBoolean(lineSplit[2]);
+
+                User user = new User(username,password,isManager);
+                users.add(user);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createInitialDatabase() {
+        try {
+            FileWriter fileWriter = new FileWriter(USER_DATABASE);
+            // Header
+            fileWriter.append("username,password,isManager").append(System.lineSeparator());
+            fileWriter.flush();
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
