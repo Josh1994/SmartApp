050e6c289795b003832b4a35ba6d66af40627860
Phil Clark <philmjc@gmail.com>
2017-05-23 18:18:47 +1200

    Update MainGUI.java

--- src/GUI/MainGUI.java
+++ src/GUI/MainGUI.java
@@ -1,17 +1,7 @@
-package GUI;
+package gui;
 
+import controller.Controller;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainGUI extends Application {
@@ -28,62 +18,19 @@ public class MainGUI extends Application {
 		// TODO Auto-generated method stub
 		primaryStage.setTitle("KPSmart");
 		
-		//Login Scene
-		Login login = new Login();
+		Controller controller  = new Controller(primaryStage);
 		
-		primaryStage.setScene(login.scene);
+		// Scenes
+		Login login = new Login(controller);
+		MailDelivery mailDelivery = new MailDelivery(controller);
+		
+		primaryStage.setScene(login.scene());
 		primaryStage.show();
 		
 	}
 	
-	static class Login implements EventHandler<ActionEvent>{
-		Button loginButton;
-		TextField usernameText;
-		PasswordField passwordText;
-		Scene scene;
-		public Login(){
-			loginButton = new Button();
-			loginButton.setText("Login");
-			loginButton.setOnAction(this);
-			
-			Label usernameLabel = new Label("Username");
-			usernameText = new TextField();
-			usernameText.setMaxHeight(10);
-			usernameText.setMaxWidth(200);
-			
-			Label passwordLabel = new Label("Password");
-			passwordText = new PasswordField();
-			
-			passwordText.setMaxHeight(10);
-			passwordText.setMaxWidth(200);
-			
-			
-		
-			VBox vbox = new VBox(10);
-			vbox.setPadding(new Insets(50, 50, 50, 50));
-			vbox.getChildren().add(usernameLabel);
-			vbox.getChildren().add(usernameText);
-			vbox.getChildren().add(passwordLabel);
-			vbox.getChildren().add(passwordText);
-			vbox.getChildren().add(loginButton);
-			
-
-			
-			scene = new Scene(vbox, 600, 400);
-		}
-		@Override
-		public void handle(ActionEvent event) {
-			// TODO Auto-generated method stub
-			if(event.getSource() == loginButton){
-				System.out.println("LoginButton");
-				System.out.println(usernameText.getText());
-				System.out.println(passwordText.getText());
-			}
-			
-		}
-		
-	}
 
 	
 	
 }
+
