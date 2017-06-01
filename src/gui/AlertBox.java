a3a1002eb34b5323afba7375f0990938158446b7
Prashant B <prashant_bhikhu@hotmail.com>
2017-06-01 13:44:09 +1200

    Updated AlertBox to be more elegant.

--- src/gui/AlertBox.java
+++ src/gui/AlertBox.java
@@ -1,10 +1,15 @@
 package gui;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -15,19 +20,35 @@ public class AlertBox {
 		
 		window.initModality(Modality.APPLICATION_MODAL);
 		window.setTitle(title);
-		window.setMinWidth(250);
-		window.setMinHeight(100);
+
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root, 300, 100, Color.WHITE);
+
+		GridPane gridpane = new GridPane();
+		gridpane.setPadding(new Insets(10));
+		gridpane.setHgap(10);
+		gridpane.setVgap(10);
 		
-		Label label = new Label();
-		label.setText(message);
+		Label label = new Label(message);
+		label.setWrapText(true);
+
+		HBox hbLabel = new HBox(10);
+		hbLabel.setAlignment(Pos.CENTER);
+		hbLabel.getChildren().add(label);
+		gridpane.add(hbLabel, 0, 0);
+
 		Button okayButton = new Button("Okay");
 		okayButton.setOnAction(e -> window.close());
-		
-		
-		VBox vbox = new VBox(15);
-		vbox.getChildren().addAll(label, okayButton);
-		vbox.setAlignment(Pos.CENTER);
-		Scene scene = new Scene(vbox);
+		okayButton.setAlignment(Pos.CENTER);
+		okayButton.setPadding(new Insets(5, 20, 5,20));
+
+		HBox hbBtn = new HBox(10);
+		hbBtn.setAlignment(Pos.CENTER);
+		hbBtn.getChildren().add(okayButton);
+		gridpane.add(hbBtn, 0, 1);
+
+		root.setCenter(gridpane);
+
 		window.setScene(scene);
 		window.showAndWait();
 		
