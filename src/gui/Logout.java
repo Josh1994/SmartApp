0fb9fe51b11d9d7289e57ad2dedbd176d8d84803
Prashant B <prashant_bhikhu@hotmail.com>
2017-06-11 17:09:15 +1200

    Updated Logout dialog layout.

--- src/gui/Logout.java
+++ src/gui/Logout.java
@@ -4,15 +4,18 @@ import controller.Controller;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  * This is a custom dialog box that is displayed when the the user clicks the logout button on any
@@ -25,8 +28,9 @@ public class Logout implements EventHandler<ActionEvent>{
 	// Global Components
     private Stage window;
     private Controller controller;
-    Button logoutButton;
-    Button cancelButton;
+
+    private Button logoutButton;
+    private Button cancelButton;
 	
     public Logout(Controller controller){
     	this.controller = controller;
@@ -41,6 +45,7 @@ public class Logout implements EventHandler<ActionEvent>{
         window.setMinHeight(100);
         
         Label bodyLabel = new Label("Do you wish to logout?");
+        bodyLabel.setPadding(new Insets(10));
 
         logoutButton = new Button("Logout");
         logoutButton.setOnAction(this);
@@ -49,12 +54,17 @@ public class Logout implements EventHandler<ActionEvent>{
         cancelButton = new Button("Cancel");
         cancelButton.setOnAction(this);
         cancelButton.setAlignment(Pos.CENTER);
-        
-        VBox vBox = new VBox(bodyLabel, logoutButton, cancelButton, new Label());
-        vBox.setSpacing(20);
 
-        vBox.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(vBox);
+        HBox hBox_Buttons = new HBox(logoutButton, cancelButton);
+        hBox_Buttons.setAlignment(Pos.CENTER_RIGHT);
+        hBox_Buttons.setSpacing(10);
+        hBox_Buttons.setPadding(new Insets(10));
+
+        BorderPane root = new BorderPane();
+        root.setCenter(bodyLabel);
+        root.setBottom(hBox_Buttons);
+
+        Scene scene = new Scene(root);
         window.setScene(scene);
         window.showAndWait();
 
@@ -63,10 +73,10 @@ public class Logout implements EventHandler<ActionEvent>{
     @Override
     public void handle(ActionEvent event) {
     	if(event.getSource().equals(cancelButton)){
-    		System.out.println("Cancel Button clicked");
+    		// System.out.println("Cancel Button clicked");
     		window.close();
     	}else if(event.getSource().equals(logoutButton)){
-    		System.out.println("Logout Button clicked");
+    		// System.out.println("Logout Button clicked");
     		controller.logout();
     		window.close();
     	}
