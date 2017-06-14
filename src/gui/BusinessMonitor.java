cfaf4b98d6e2998ed2ab5202423b139088e91219
Joshua Mina <minajosh@pulcinella.ecs.vuw.ac.nz>
2017-06-14 13:13:13 +1200

    Fixed merge error in Event Manager

--- src/gui/BusinessMonitor.java
+++ src/gui/BusinessMonitor.java
@@ -85,7 +85,6 @@ public class BusinessMonitor{
 
 		vbox.getChildren().addAll(revBox, expBox, evntBox, mailBox, avgBox, critBox);
 	}
-<<<<<<< HEAD
 
 	public void display(Route route){
 
@@ -113,41 +112,6 @@ public class BusinessMonitor{
 		//vbox.getChildren().addAll(vbox);
 		//disBox.getChildren().addAll(revBox, expBox, evntBox, mailBox, avgBox, critBox);
 	}
-
-=======
-	
-	
-	public void setRevenue(String revenue){
-		this.revenueLabel = revenue;
-	}
-	
-	public void setExpenditure(String exp){
-		this.expLabel = exp;
-	}
-	
-	public void setCritLabels(ArrayList<String> critLabels){
-		this.critLabels = critLabels;
-	}
-	
-	public void setNumEvents(String numEv){
-		this.numEvLabel = numEv;
-	}
-	
-	public void setMailAmount(String amount){
-		this.mailAmountLabel = amount;
-	}
-	
-	public void setAveDelTime(String avgDelTime){
-		this.avgDelTimeLabel = avgDelTime;
-	}
-	
-	public void setCritLabel(String critLabel){
-		this.critLabel = critLabel;
-	}
-	
-	
-	
->>>>>>> 89f1843b14f91a58a1f54c4aac2d92ca5fbfe6bb
 	public VBox vbox(){
 
 		return vbox;
